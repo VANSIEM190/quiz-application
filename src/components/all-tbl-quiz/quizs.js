3d5c79433037ab6d8a8a72e240728b0f9076e3b0
VANSIEM190 <vansiemgracia03@gmail.com>
2025-01-25 01:41:57 +0100

    update file

---
+++ src/components/all-tbl-quiz/quizs.js
@@ -0,0 +1,13 @@
+import * as DomaineScolaire from '../questions/domain scolaire/all-quiz-domaine-scolaire'
+import * as DomaineCommerciale from '../questions/domaine commercial/all-quiz-domaine-commersiale'
+import * as DomaineSport from '../questions/domaine sport/all-quiz-domaine-sport'
+import * as DomaineReligion from '../questions/domaine religion/all-quiz-religion'
+import * as DomaineCinema from '../questions/domaine cinema/all-quiz-cinema'
+
+export {
+  DomaineScolaire,
+  DomaineCommerciale,
+  DomaineSport,
+  DomaineReligion,
+  DomaineCinema,
+}
