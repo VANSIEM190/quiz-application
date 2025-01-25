55f47e995f898147bd06c912dbbd53f8ddbafe81
VANSIEM190 <vansiemgracia03@gmail.com>
2025-01-25 01:34:25 +0100

    update file

--- src/components/domaines/all-domaine.js
+++ src/components/domaines/all-domaine.js
@@ -3,7 +3,6 @@ import Domaine_Commerciale from '../domaines/domaine-commercial'
 import Domaine_Sport from '../domaines/domaine-sport'
 import Domaine_Religion from '../domaines/domaine-religion'
 import Domaine_Cinema from '../domaines/domaine-cinema'
-import Domaine_Jeux_Videos from '../domaines/domain-jeux-vidéo'
 
 export {
   Domaine_Scolaire,
@@ -11,5 +10,4 @@ export {
   Domaine_Sport,
   Domaine_Religion,
   Domaine_Cinema,
-  Domaine_Jeux_Videos,
 }
